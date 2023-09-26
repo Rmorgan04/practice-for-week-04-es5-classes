226b8f24119e19eb6a40445320d88cbbb480b7d8
Robby Morgan <robby.d.morgan@gmail.com>
2023-09-26 16:19:10 -0500

    finished project

--- classes/array.js
+++ classes/array.js
@@ -1,1 +1,12 @@
-// Your code here
+Array.prototype.isEqual = function(array) {
+   for (let i = 0; i < array.length; i++) {
+    let el = this[i];
+    if (el !== array[i]) {
+        return false;
+    }
+   }
+   return true;
+   
+}
+
+console.log([1, 2, 3,].isEqual([1, 2, 3]));
